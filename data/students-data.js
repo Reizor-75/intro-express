668486532a846e8251133dbb43cb104617011a19
mlinares <m.linares93@gmail.com>
2024-01-09 09:07:36 -0500

    inital commit

---
+++ data/students-data.js
@@ -0,0 +1,20 @@
+const students = [
+  {id: 1, name: "Michelle", pronouns: "she/her"},
+  {id: 2, name: "Drew", pronouns: "He/him"},
+  {id: 3, name: "Jia", pronouns: "Not avaiable"},
+  {id: 4, name: "Nat", pronouns: "They/Them"},
+  {id: 5, name: "Teron", pronouns: "Not available"},
+  {id: 6, name: "Rich B", pronouns: "He/Him"},
+  {id: 7, name: "Tommy", pronouns: "He/Him"},
+  {id: 7, name: "Arya", pronouns: "He/Him"},
+  {id: 7, name: "Bri", pronouns: "she/Her"},
+  {id: 7, name: "Rich H", pronouns: "He/Him"},
+  {id: 7, name: "Asa", pronouns: "Not available"},
+  {id: 7, name: "Matthew", pronouns: "He/Him"},
+  {id: 7, name: "Kyristina", pronouns: "available"},
+  {id: 7, name: "Shawn", pronouns: "Not available"},
+]
+
+export{
+  students
+}
---
+++ server.js
@@ -0,0 +1,35 @@
+// important modules
+import express from "express";
+import { todos } from "./data/todo-data.js"; 
+//create Express app
+const app = express();
+
+//configure the app (app.set)
+app.set("view engine", "ejs");
+//mount Middleware (app.use)
+
+//mount routes 
+app.get("/", function(req, res){
+  // '/' route to a specific location
+  //- The request (`req`) object has properties and methods to access information regarding the current HTTP *request*, including any data the browser/tool has sent.
+  // - The response (`res`) object contains properties and methods used to *end the request/response cycle* - like we’ve done so far using the `res.send` method.
+  // res.send("<h1> hello Express</h1>"); 
+  
+  res.redirect("/home");
+});
+
+app.get("/home", function(req, res){
+  res.render("home");
+})
+
+app.get("/todos", function(req, res){
+  res.render("todos/index", {
+    myName: "Michelle",
+    favFood: "tofu",
+    todos: todos
+  });
+})
+//tell the app to listen on port 3000
+app.listen(3000, function(){
+  console.log("Listening on port 3000");
+});
