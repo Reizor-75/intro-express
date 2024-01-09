e4dd15fed79ad0c06eb6a1b1e18202fe1c5d3c27
mlinares <m.linares93@gmail.com>
2024-01-09 09:18:28 -0500

    uppdate student data

--- data/students-data.js
+++ data/students-data.js
@@ -1,19 +1,20 @@
 const students = [
   {id: 1, name: "Michelle", pronouns: "she/her"},
   {id: 2, name: "Drew", pronouns: "He/him"},
-  {id: 3, name: "Jia", pronouns: "Not avaiable"},
+  {id: 3, name: "Jia", pronouns: "Not available"},
   {id: 4, name: "Nat", pronouns: "They/Them"},
   {id: 5, name: "Teron", pronouns: "Not available"},
   {id: 6, name: "Rich B", pronouns: "He/Him"},
   {id: 7, name: "Tommy", pronouns: "He/Him"},
-  {id: 7, name: "Arya", pronouns: "He/Him"},
+  {id: 7, name: "Arya", pronouns: "She/Her"},
   {id: 7, name: "Bri", pronouns: "she/Her"},
-  {id: 7, name: "Rich H", pronouns: "He/Him"},
+  {id: 7, name: "Rich H", pronouns: "Not available"},
   {id: 7, name: "Asa", pronouns: "Not available"},
   {id: 7, name: "Matthew", pronouns: "He/Him"},
-  {id: 7, name: "Kyristina", pronouns: "available"},
+  {id: 7, name: "Kyristina", pronouns: "Not available"},
   {id: 7, name: "Shawn", pronouns: "Not available"},
 ]
+//Info taken from Slack profiles
 
 export{
   students
