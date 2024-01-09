f00065d145b74b55b4a621958bb155c8aa2fef12
mlinares <m.linares93@gmail.com>
2024-01-09 09:24:52 -0500

    import student data to server.js, add app.listen statment

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // important modules
 import express from "express";
 import { todos } from "./data/todo-data.js"; 
+import { students } from "./data/students-data.js";
 //create Express app
 const app = express();
 
+//todos
+/*
 //configure the app (app.set)
 app.set("view engine", "ejs");
 //mount Middleware (app.use)
@@ -33,3 +36,24 @@ app.get("/todos", function(req, res){
 app.listen(3000, function(){
   console.log("Listening on port 3000");
 });
+*/
+
+//students
+// configure the app (app.set)
+
+
+
+// mount Middleware (app.use)
+
+
+
+// mount routes
+
+
+
+// tell the app to listen on port 3000
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000');
+});
+
